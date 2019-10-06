a2b4ab6609ecf441546518fa187fc8cb8e5e1562
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-06 10:59:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
 # code your #position_taken? method here!
 
-
-
 def position_taken?(board, index) #  method position taken accepts the current board and checks the position the player would like to go is empty
   if (board[index] == " ") || (board[index] == "") || (board[index] == nil) # if either a blank space, no space or nil entry is present then the board[index] aka position the player wants to go is empty
        return false # this means the board [index] does not have a value so the player is free to move here
